d44430036e94f26ffbe1fd841b3db881c682b401
Pedro Paulo <pedropaulo28122004@gmail.com>
2025-04-06 17:05:31 -0300

    V1

--- Los Lobos C#/Trabalho 3 Camadas/InicioForm.Designer.cs
+++ Los Lobos C#/Trabalho 3 Camadas/InicioForm.Designer.cs
@@ -32,7 +32,6 @@
             menuStrip1 = new MenuStrip();
             mENUToolStripMenuItem = new ToolStripMenuItem();
             cadastrarClienteToolStripMenuItem1 = new ToolStripMenuItem();
-            cadastrarPetToolStripMenuItem = new ToolStripMenuItem();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -49,7 +48,7 @@
             // mENUToolStripMenuItem
             // 
             mENUToolStripMenuItem.CheckOnClick = true;
-            mENUToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cadastrarClienteToolStripMenuItem1, cadastrarPetToolStripMenuItem });
+            mENUToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cadastrarClienteToolStripMenuItem1 });
             mENUToolStripMenuItem.Name = "mENUToolStripMenuItem";
             mENUToolStripMenuItem.Size = new Size(65, 24);
             mENUToolStripMenuItem.Text = "MENU";
@@ -63,14 +62,6 @@
             cadastrarClienteToolStripMenuItem1.Size = new Size(252, 26);
             cadastrarClienteToolStripMenuItem1.Text = "Cadastrar Cliente";
             cadastrarClienteToolStripMenuItem1.Click += cadastrarClienteToolStripMenuItem1_Click;
-            // 
-            // cadastrarPetToolStripMenuItem
-            // 
-            cadastrarPetToolStripMenuItem.Name = "cadastrarPetToolStripMenuItem";
-            cadastrarPetToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.P;
-            cadastrarPetToolStripMenuItem.Size = new Size(252, 26);
-            cadastrarPetToolStripMenuItem.Text = "Cadastrar Pet";
-            cadastrarPetToolStripMenuItem.Click += cadastrarPetToolStripMenuItem_Click;
             // 
             // InicioForm
             // 
@@ -98,6 +89,5 @@
         private MenuStrip menuStrip1;
         private ToolStripMenuItem mENUToolStripMenuItem;
         private ToolStripMenuItem cadastrarClienteToolStripMenuItem1;
-        private ToolStripMenuItem cadastrarPetToolStripMenuItem;
     }
 }
